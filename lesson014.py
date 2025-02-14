4b7cc45028d2ff293b163b6acbc587bdd01c97a9
Mark Peters <mapeters101@earthlink.net>
2025-02-14 11:04:16 -0500

    Work on lesson014 to 2:45.

---
+++ lesson014.py
@@ -0,0 +1,24 @@
+def func_with_args_and_kwargs(*args, **kwargs):
+    print(f"Arguments were: {args}")
+    print(f"Type of args: {type(args)}")
+
+    print(f"Keyword Arguments were: {kwargs}")
+    print(f"Type of args: {type(kwargs)}")
+
+def common_function(age, name, job, **kwargs):
+    formatted_info = {
+        'age': age,
+        'name': name,
+        'job': job,
+        'description': f"{name} is a {age} year old {job}.",
+        'additional_info': kwargs # NESTED DICTIONARY
+    }
+    return formatted_info
+
+if __name__ == '__main__':
+
+    age, name, job = 30, 'Jane', 'Software Developer'
+
+    print(common_function(age, name, job, experience=10, company='Google'))
+
+
