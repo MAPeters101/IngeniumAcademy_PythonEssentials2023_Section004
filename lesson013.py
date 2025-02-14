df342970e28aa8fc3eb4cde9d0461bb745606d13
Mark Peters <mapeters101@earthlink.net>
2025-02-13 23:00:55 -0500

    Work on lesson013 to 2:30.

---
+++ lesson013.py
@@ -0,0 +1,18 @@
+def format_info(age, name, job):
+    formatted_info = {
+        'age': age,
+        'name': name,
+        'job': job,
+        'description': f"{name} is a {age} year old {job}."
+    }
+    return formatted_info
+
+if __name__ == '__main__':
+    age, name, job =  30, 'John', 'Software Developer'
+
+    formatted_data = format_info(age=age, name=name, job=job)
+    print(formatted_data)
+
+
+
+
